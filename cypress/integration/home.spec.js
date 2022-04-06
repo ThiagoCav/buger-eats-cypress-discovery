2d478eb6a60d34e24901682337c7f955cba6e3a4
ThiagoCav <thiagocavallari@outlook.com>
2022-04-06 17:08:27 -0300

    ajustando a suite

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,6 @@
 describe('Home page', ()=> {
     it('app deve estar online', ()=>{
-        cy.viewport(1440, 900)
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
         cy.get('#page-home main p').should('have.text', 'Em vez de oportunidades tradicionais de entrega de refeições em horários pouco flexíveis, seja seu próprio chefe.')
         
